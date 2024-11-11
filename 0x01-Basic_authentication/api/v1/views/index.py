23dcc380eb39de0e83c01127e461c1d925576ebf
Est-1y <terryiana22@gmail.com>
2024-11-11 17:18:39 +0000

    authentication

---
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Module"""
+from flask import jsonify, abort
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET status"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /stats/"""
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
+
+
+@app_views.route("/unauthorized/",
+                 strict_slashes=False)
+def unauthorized() -> str:
+    """unauthorized req"""
+    abort(401)
+
+
+@app_views.route("/forbidden/",
+                 strict_slashes=False)
+def forbidden() -> str:
+    """forbidden requests"""
+    abort(403)
